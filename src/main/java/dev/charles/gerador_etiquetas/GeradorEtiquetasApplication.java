ce3994cbd83cbd14e5ff1ed55df93536ca7ebdf9
Charles Magalhães <charles.magalhaes.developer@gmail.com>
2026-06-10 23:01:48 -0300

    Inicializa projeto gerador de etiquetas

---
+++ src/main/java/dev/charles/gerador_etiquetas/GeradorEtiquetasApplication.java
@@ -0,0 +1,13 @@
+package dev.charles.gerador_etiquetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorEtiquetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorEtiquetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/charles/gerador_etiquetas/GeradorEtiquetasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.charles.gerador_etiquetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorEtiquetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
